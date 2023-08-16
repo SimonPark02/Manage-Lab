6d6115f4dd4a513de65437677a128f9e4757367e
SimonPark02 <yjunpark0226@gmail.com>
2023-08-16 11:30:22 +0900

    Minor change

--- manage/__main__.py
+++ manage/__main__.py
@@ -1,17 +1,17 @@
 import argparse
-from manage_lab import mklab, rmlab
+from lib.manage_lab import mklab, rmlab
 
 # top-level parser
-parser = argparse.ArgumentParser(prog="python manage", description="Manage Lab folders")
+parser = argparse.ArgumentParser(prog="python manage", description="Managament programs for lazy people")
 subparsers = parser.add_subparsers(title="commands", dest="command")
 
 # create parser for "mklab" command
 mklab_parser = subparsers.add_parser("mklab", help="Create new lab folder")
-mklab_parser.add_argument("Name", type=str, help="name of the lab")
+mklab_parser.add_argument("Name", type=str, help="Name of the lab")
 
 # create parser for "rmlab" command
 rmlab_parser = subparsers.add_parser("rmlab", help="Delete lab folder")
-rmlab_parser.add_argument("Name", type=str, help="name of the lab")
+rmlab_parser.add_argument("Name", type=str, help="Name of the lab")
 
 args = parser.parse_args()
 
--- manage/lib/manage_lab.py
+++ manage/lib/manage_lab.py
@@ -1,15 +1,14 @@
 import os
 import sys
-from setting import (LAB_PATH, LAB_FOLDERS)
+from lib.path import (LAB_PATH, LAB_FOLDERS)
 
 def mklab(name):
     for f in LAB_FOLDERS:
         try:
             os.makedirs(os.path.join(LAB_PATH, name, f))
-            print(f"/{f} Successfully created in /{name}")
+            print(f"{f} Successfully created in {os.path.join(LAB_PATH, name)}")
         except FileExistsError:
-            print(f"/{f} Already exists in /{name}")
-    print("May the force be with you ...")
+            print(f"{f} Already exists in {os.path.join(LAB_PATH, name)}")
 
 def rmlab(name):
     _ask(f"Do you really want to remove folder {name}? [y/n]: ")
